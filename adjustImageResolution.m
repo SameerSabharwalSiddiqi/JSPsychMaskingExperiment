de1d1c915fa36faf44992fbfd9f5feac7c50df8c
Sameer Siddiqi <ssabhsiddiqi@gmail.com>
2021-02-26 15:38:45 -0500

    committing before adding Elizabeth's revision, namely, to remove the one-question sliding scale and replace with two questions, is it congruent or incongruent? And, how confident are you?

--- adjustImageResolution.m
+++ adjustImageResolution.m
@@ -1,16 +1,16 @@
-CFDfaces = dir(fullfile('jspsych\CFDfaces','*.JPG'));
+CFDfaces = dir(fullfile('jspsych\supplementaryImages','*.JPG'));
 
-mkdir('jspsych\resizedCFDfaces')
-saveDir = 'jspsych\resizedCFDfaces';
+mkdir('jspsych\supplementaryImages')
+saveDir = 'jspsych\supplementaryImages';
 
 nCFDfaces = length(CFDfaces);
 
 for i = 1:nCFDfaces
     %select and scramble face
     face = CFDfaces(i).name;
-    faceDir = fullfile('jspsych/CFDfaces',face);
+    faceDir = fullfile('jspsych/supplementaryImages',face);
     img = imread(faceDir); %vertically flipped for sanity check
-    resizedImg = imresize(img,0.25);
+    resizedImg = imresize(img,0.5);
     fullSaveDir = fullfile(saveDir,face);
     imwrite(resizedImg,fullSaveDir,'JPG');
 end
